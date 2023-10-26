5162a3206853c029e061b6d11227b7347746c78a
root <lsgindfw@gmail.com>
2023-10-26 14:55:19 -0400

    committed

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,9 +1,22 @@
 class Character:
-    #str default_name
-    #str name = " "
+    default_name = None
 
-    def __init__(self, character_name):
+    def __init__(self, name, default_name):
+        self.naem = name
+        Chracter.default_name = default_name
+        self.position = None 
     
+    def get_name(self):
+        return self.name
+    
+    def get_total_positions(self):
+        return self.position
+    
+    def enter_map(self, game_map):
+        return game_map
+    
+    def move(self, direction):
+        return direction
 
         
     
