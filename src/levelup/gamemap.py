22b3a9efa56c063cd807fe33534197f136f038fb
xykat <saikatgupta@gmail.com>
2023-10-26 15:02:43 -0400

    Created Game-Map class

---
+++ src/levelup/gamemap.py
@@ -0,0 +1,25 @@
+class gamemap:
+
+    def getPositions(self):
+        print("This is classa method.")
+
+    def calculateValidatePositions(self):
+        print("This is classb method.")
+
+    def isValidPOsition(self):
+        print("This is classc method.")
+
+    def getTotalPositions(self):
+        print("This is classc method.")
+
+
+
+
+
+
+
+
+    name = ""
+
+    def __init__(self, character_name):
+        self.name = character_name
