91c4694b078c958de5a9583da23838f660d8768c
root <l_peaches@msn.com>
2023-10-26 17:14:03 -0400

    ADDED POSTION CODE

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,3 +1,18 @@
-class Position
-def startingPosition[]
+class Position ():
+
+    x = -100
+    y = -100
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, obj):
+        if self.x == obj.x and self.y == obj.y:
+            return True
+        else:
+            return False
+
+
+
     
