41d1e5b3bb3855f94eda971f193ed61247e7c96d
root <lsgindfw@gmail.com>
2023-10-26 15:32:53 -0400

    Update Character Class

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,22 +1,14 @@
 class Character:
-    default_name = None
+    def __init__(self):
+        self.name = None  # Initialize the name attribute
+    
+    def get_user_input(self, prompt):
+        user_input = input(prompt)
+        return user_input
 
-    def __init__(self, name, default_name):
-        self.naem = name
-        Chracter.default_name = default_name
-        self.position = None 
-    
-    def get_name(self):
-        return self.name
-    
-    def get_total_positions(self):
-        return self.position
-    
-    def enter_map(self, game_map):
-        return game_map
-    
-    def move(self, direction):
-        return direction
+# Create an instance of the class
+input_handler = Character()
 
-        
-    
+# Show to the user and collect the data
+user_text = input_handler.get_user_input("Enter the name of your character: ")
+print("You entered: ", user_text)
