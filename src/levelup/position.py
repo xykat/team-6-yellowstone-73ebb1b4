c50b294521c83115e73ed1f308a5cc7ed1266de8
root <l_peaches@msn.com>
2023-10-26 17:34:58 -0400

    ADD POSITION TEST CASE

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -4,8 +4,8 @@ class Position ():
     y = -100
 
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+        self.x = x # X-coordinates 
+        self.y = y # Y-coordinates 
 
     def __eq__(self, obj):
         if self.x == obj.x and self.y == obj.y:
