8ab8127577be7bf9d92d6b10e9e44c8d902b6425
xykat <saikatgupta@gmail.com>
2023-10-27 11:35:09 -0400

    Added test case for Game map

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -1,30 +1,20 @@
-class gamemap:
+class GameMap:
 
     def getPositions(self):
         print("This is getPositions method.")
 
     def calculateValidatePositions(self):
-        GRID_SIZE = 10
-    while True:
-        try:
-            # Get user input for the X and Y coordinates
-            x = int(input(f"Enter the X coordinate (0 to {GRID_SIZE-1}): "))
-            y = int(input(f"Enter the Y coordinate (0 to {GRID_SIZE-1}): "))
-
-            if direction:'NORTH'
-            y +=1
-            if 0 <= y < GRID_SIZE:
-                return y
-            else: y -=1
-
-            # Check if the coordinates are within the grid boundaries
-            if 0 <= x < GRID_SIZE and 0 <= y < GRID_SIZE:
-                return x, y
-            else:
-                print("Coordinates are out of bounds. Please try again.")
-        except ValueError:
-            print("Invalid input. Please enter integers for coordinates.")
     
+        while True:
+            try:
+            # Get user input for the X and Y coordinates
+                x = int(input(f"Enter the X coordinate (0 to {GRID_SIZE-1}): "))
+                y = int(input(f"Enter the Y coordinate (0 to {GRID_SIZE-1}): "))
 
-    def get_position(self):
-        print("This is get_position method.")
+                if direction:'NORTH'
+                y +=1
+                if 0 <= y < 10:
+                    y=y
+                else: y -=1
+            except ValueError:
+                print("Invalid input. Please enter integers for coordinates.")
---
+++ tests/test_gamemap.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from levelup.character import Character
+from levelup.controller import Direction
+from levelup.gamemap import GameMap
+from levelup.position import Position
+
+class TestGameMap(TestCase):
+    
+    def test_getPositions(self):
+        testobj = GameMap
+
+    def test_calculateValidatePositions(self):
+        testobj= GameMap
