4f03aff8dccccd0a16ff58aed7c1292d69da661d
root <l_peaches@msn.com>
2023-10-27 11:41:38 -0400

    UPDATED POSITION TEST

--- tests/test_position.py
+++ tests/test_position.py
@@ -4,8 +4,8 @@ from levelup.position import Position
 
 class TestPositionInitWithXY(TestCase):
     def test_init(self):
-        ARBITRARY_X = 3
-        ARBITRARY_Y = 4
-        testobj = Position(3,4)
-        self.assertEqual(ARBITRARY_X, testobj.x)
-        self.assertEqual(ARBITRARY_Y, testobj.y)
+        x = 2
+        y = 4
+        testobj = Position(x, y)
+        self.assertEqual(x, testobj.x)
+        self.assertEqual(y, testobj.y)
